f8085c0820abb6f1eab8f01c007da090d90c4150
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-11-11 22:43:59 -0800

    spotlight sum

--- square each digit.js
+++ square each digit.js
@@ -1,3 +1,10 @@
 function squareDigits(n) {
 return 	+(n.toString().split("").map(x=>x**2).join(""));
 }
+function numberSplit(n) {
+	return [Math.floor(n/2), Math.ceil(n/2)]
+}
+function spotlightSum(n) {
+	let y = 8*n;
+	return y + n
+}
