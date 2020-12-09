612611cfc26d0e706542f6f2b51c3ca043c50d4d
mcube25 <mcube3567@gmail.com>
2020-12-09 09:29:23 -0800

    destructuring arrays

--- rest parameter.js
+++ rest parameter.js
@@ -1,3 +1,38 @@
 const sum = (...args) => {
   return args.reduce((a, b) => a + b, 0);
 }
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const {today} = HIGH_TEMPERATURES
+const {tomorrow} = HIGH_TEMPERATURES
+
+// Only change code above this line
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const { today: highToday} = HIGH_TEMPERATURES;
+const {tomorrow: highTomorrow} = HIGH_TEMPERATURES;
+
+// Only change code above this line
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
+
+const {today: {low: lowToday}} = LOCAL_FORECAST;
+const {today: {high: highToday}} = LOCAL_FORECAST;
