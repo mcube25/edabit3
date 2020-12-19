68cbddd8b1b6df5399c39dee880229a02743a562
mcube25 <mcube3567@gmail.com>
2020-12-18 16:43:28 -0800

    setter and getter

--- template literals.js
+++ template literals.js
@@ -34,3 +34,19 @@ const bicycle = {
 // Only change code above this line
 bicycle.setGear(3);
 console.log(bicycle.gear);
+
+
+
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
+  }
+
+  get temperature() {
+    return (5 / 9) * (this.fahrenheit - 32);
+  }
+
+  set temperature(celsius) {
+    this.fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
+}
