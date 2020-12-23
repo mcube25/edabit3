89e334c964a68cde5ce6dd7eb20e59033da71dbc
mcube25 <mcube3567@gmail.com>
2020-12-22 18:26:02 -0800

    regex

--- promise.js
+++ promise.js
@@ -1,5 +1,38 @@
-let promise = new Promise( (resolve, reject) => {
-    setTimeout(( ) => {
-       resolve("edabit")
-    }, 1000)
-  })
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+
+  if(responseFromServer) {
+    resolve('We got the data')
+  } else {
+    reject('Data not received')
+  }
+});
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+
+  if(responseFromServer) {
+    resolve("We got the data")
+    .then(result => {console.log(result)});
+  } else {
+    reject("Data not received");
+  }
+});
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {
+    reject("Data not received").catch(error => {
+  console.log(error)
+});;
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
---
+++ regex2.js
@@ -0,0 +1,17 @@
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
