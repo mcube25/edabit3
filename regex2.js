8484287465a2ec1d430ffef73ad6de5315e307d3
mcube25 <mcube3567@gmail.com>
2020-12-23 18:34:35 -0800

    regex

--- regex2.js
+++ regex2.js
@@ -15,3 +15,7 @@ let result = extractStr.match(codingRegex); // Change this line
 let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /twinkle/gi; // Change this line
 let result = twinkleStar.match(starRegex); // Change this line
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
