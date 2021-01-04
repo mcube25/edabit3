3b51a61c1273d776fc4afe382322ce144a84e3ed
mcube25 <mcube3567@gmail.com>
2021-01-03 23:15:09 -0800

    data structure

---
+++ data structure.js
@@ -0,0 +1,7 @@
+let yourArray=['yes', 1, true, undefined, {m:'cube'}]; // Change this line
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = 'hey'
+// Only change code above this line
+console.log(myArray);
