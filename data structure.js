bd3ccfa895f004a45db2d61bf7bae1a2f44c22c2
mcube25 <mcube3567@gmail.com>
2021-01-06 23:33:21 -0800

    data structure4

--- data structure.js
+++ data structure.js
@@ -15,3 +15,11 @@ arr.unshift("I", 2, "three");
 }
 
 console.log(mixedNumbers(['IV', 5, 'six']))
+
+function popShift(arr) {
+  let popped = arr.pop()// Change this line
+  let shifted= arr.shift(); // Change this line
+  return [shifted, popped];
+}
+
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
