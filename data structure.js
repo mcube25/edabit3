729175f055a493300554ee71497f4491f235a307
mcube25 <mcube3567@gmail.com>
2021-01-13 23:21:18 -0800

    data structure8

--- data structure.js
+++ data structure.js
@@ -94,3 +94,13 @@ for (let i = 0; i< arr.length; i++){
 }
 
 console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+
+let myNestedArray = [
+  // change code below this line
+  ["unshift", false, 1, 2, 3, "complex", "nested"],
+  ["loop", "shift", 6, 7, 1000, "method"],
+  ["concat", false, true, "spread", "array", ["deep"]],
+  ["mutate", 1327.98, "splice", "slice", "push", [["deeper"]]],
+  ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth", [[["deepest"]]]]
+  // change code above this line
+];
