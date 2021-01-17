e01f556f458579b114e0eea32d13b5536ab3cabd
mcube25 <mcube3567@gmail.com>
2021-01-16 23:44:00 -0800

    data structure12

--- data structure.js
+++ data structure.js
@@ -135,3 +135,21 @@ userActivity.data.online = 45;
 // Only change code above this line
 
 console.log(userActivity);
+
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+  // Only change code below this line
+return foods[scannedItem]
+  // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
