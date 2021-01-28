1ec8c5ae35af01aa14f80474c8ea8040a6eea132
mcube25 <mcube3567@gmail.com>
2021-01-28 00:24:36 -0800

    http module

--- mynode.js
+++ mynode.js
@@ -16,3 +16,19 @@ http.createServer(function (req, res){
   res.write('the time is ' + dt.myDatetime());
   res.end();
 }).listen(8080);
+//built in http module
+//to include the hhtp module we use the require() method
+//use the createServerO() method to create a http server
+var http = require('http');
+http.createServer(function (req, res){
+  res.writeHead(200, {'content':'text.html'});
+  res.write ('hello world');
+}).listen(8080);
+//  the function passed into the hhtp create server has a req argument that represents the request from the client
+var http = require ('http');
+http.createServer(function (req, res){
+  res.writeHead(200, {'content-type':'text.html'});
+  res.write(req.url);
+  res.end();
+}).listen(8080);
+//split query string into readable parts
