e57e7945190167c1a7d9040a9351400b774abd78
mcube25 <mcube3567@gmail.com>
2021-01-27 23:08:32 -0800

    modules

--- mynode.js
+++ mynode.js
@@ -5,3 +5,14 @@ http.createServer (function (req, res){
 }).listen(8080);
 
 //create a module
+exports.myDatetime = function (){
+  return Date()
+};
+// now you can include and use your own module
+var http = require ('http');
+var dt = require ('./module address');
+http.createServer(function (req, res){
+  res.writeHead(200, {'content-type':'text.html'});
+  res.write('the time is ' + dt.myDatetime());
+  res.end();
+}).listen(8080);
