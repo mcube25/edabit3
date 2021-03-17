2da7d43e3205531009a3979e98c95d516f4fa32b
mcube25 <mcube3567@gmail.com>
2021-03-17 00:49:47 -0700

    memoization

--- memoize.js
+++ memoize.js
@@ -1,1 +1,17 @@
-//
+
+
+function memo(arg) {
+    if (!memo.cache) {
+        memo.cache = {}
+
+    }
+    if (!memo.cache[arg]) {
+      let result={};
+      result = arg;
+        return memo.cache[arg] = result
+
+    }
+    return memo.cache[arg]
+}
+
+//2
